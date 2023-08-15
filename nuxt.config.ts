db8fc5362f9cc63a43c80fb9b178b7017ba342fb
ismael tismatek <i.hini@cultureg.eu>
2023-08-15 21:47:12 +0200

    remove pwa config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,45 +38,45 @@ export default defineNuxtConfig({
 //     }
 // },
   pwa: {
-    registerType: 'autoUpdate',
-    manifest: {
-      name: 'MONAPARTE PWA',
-      short_name: 'monaparte_pwa',
-      theme_color: '#ffffff',
-      icons: [
-        {
-          src: 'pwa-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-        },
-        {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any maskable',
-        },
-      ],
-    },
-    workbox: {
-      navigateFallback: '/',
-      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
-    },
-    client: {
-      installPrompt: true,
-      // you don't need to include this: only for testing purposes
-      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
-      periodicSyncForUpdates: 20,
-    },
-    devOptions: {
-      enabled: true,
-      suppressWarnings: true,
-      navigateFallbackAllowlist: [/^\/$/],
-      type: 'module',
-    },
+    // registerType: 'autoUpdate',
+    // manifest: {
+    //   name: 'MONAPARTE PWA',
+    //   short_name: 'monaparte_pwa',
+    //   theme_color: '#ffffff',
+    //   icons: [
+    //     {
+    //       src: 'pwa-192x192.png',
+    //       sizes: '192x192',
+    //       type: 'image/png',
+    //     },
+    //     {
+    //       src: 'pwa-512x512.png',
+    //       sizes: '512x512',
+    //       type: 'image/png',
+    //     },
+    //     {
+    //       src: 'pwa-512x512.png',
+    //       sizes: '512x512',
+    //       type: 'image/png',
+    //       purpose: 'any maskable',
+    //     },
+    //   ],
+    // },
+    // workbox: {
+    //   navigateFallback: '/',
+    //   globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    // },
+    // client: {
+    //   installPrompt: true,
+    //   // you don't need to include this: only for testing purposes
+    //   // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+    //   periodicSyncForUpdates: 20,
+    // },
+    // devOptions: {
+    //   enabled: true,
+    //   suppressWarnings: true,
+    //   navigateFallbackAllowlist: [/^\/$/],
+    //   type: 'module',
+    // },
   },
 })
